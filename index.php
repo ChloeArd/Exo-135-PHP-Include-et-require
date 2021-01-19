01aacc0c29434e54835de27c2c53f84eece0dc1c
ChloeArd <chloe.ardoise@gmail.com>
2021-01-19 09:33:35 +0100

    Exercice 135

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
 
 echo "Un fichier php va être inclu à la suite de ce texte ...<br><br>";
 
+
 // inclure fichier texte.php ci dessous
+?>
+<?php
+include "./texte.php";
+?>
 
 
+<?php
 // Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
-
-
-
-echo "J'aime également les " . $maVar;
-
+?>
+<?php
+include "./vars.php";
+echo "J'aime également les $maVar";
+?>
 
 
 
